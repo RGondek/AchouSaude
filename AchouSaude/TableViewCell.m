6b51927098c679ac2157c9f9a02ca4bca848e687
Rubens <rubens.gondek@gmail.com>
2015-02-26 17:17:29 -0300

    Fluxo de Telas

---
+++ AchouSaude/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  AchouSaude
+//
+//  Created by Rubens Gondek on 2/26/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ AchouSaude/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  AchouSaude
+//
+//  Created by Rubens Gondek on 2/26/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
