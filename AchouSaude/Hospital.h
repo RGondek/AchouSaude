3ddcb03a2d8b0de94a38ab0d586bd17c8652597c
Rubens <rubens.gondek@gmail.com>
2015-03-02 17:49:25 -0300

    Criado classe Hospital

--- AchouSaude/Hospital.h
+++ AchouSaude/Hospital.h
@@ -2,14 +2,17 @@
 //  Hospital.h
 //  AchouSaude
 //
-//  Created by Rubens Gondek on 2/27/15.
+//  Created by Rubens Gondek on 3/2/15.
 //  Copyright (c) 2015 Ckode. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
 
-@interface Hospital : NSObject{
-    
-}
+@interface Hospital : MKAnnotationView <MKAnnotation>
+
+@property NSString* address;
+@property NSMutableArray* convenios;
+@property NSArray* phone;
+@property NSArray* time;
 
 @end
--- AchouSaude/Hospital.m
+++ AchouSaude/Hospital.m
@@ -2,12 +2,22 @@
 //  Hospital.m
 //  AchouSaude
 //
-//  Created by Rubens Gondek on 2/27/15.
+//  Created by Rubens Gondek on 3/2/15.
 //  Copyright (c) 2015 Ckode. All rights reserved.
 //
 
 #import "Hospital.h"
 
 @implementation Hospital
+
+@synthesize title, image, address, convenios, phone, time, coordinate;
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
 
 @end
