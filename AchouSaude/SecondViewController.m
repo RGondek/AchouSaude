b569f40a6ce9593ce5ee2a14a25e9170768e9f2c
mmedeiro <31313132@mackezista.com.br>
2015-03-03 14:51:19 -0300

    Add associação de hospitais e planos de saúde

--- AchouSaude/SecondViewController.m
+++ AchouSaude/SecondViewController.m
@@ -21,8 +21,17 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     convenioHosp = [[NSMutableArray alloc] init];
-    convenios = @[@"AMIL",@"UNIMED"];
+    convenios = @[@"ALIANZ",@"CARE PLUS", @"GOLDEN CROSS", @"DIX AMIL", @"MEDIAL", @"MARITIMA", @"VOLKSWAGEN"];
     [convenioHosp addObject:convenios];
+    convenios = @[@"ALIANZ",@"BRADESCO",@"GOLDEN CROSS", @"DIX/AMIL",@"INTERMEDICA", @"MARITIMA", @"VOLKSWAGEN"];
+    [convenioHosp addObject:convenios];
+    convenios = @[@"ALIANZ",@"AMIL",@"AMIL BLUE",@"BRADESCO",@"GOLDEN CROSS", @"DIX/AMIL",@"INTERMEDICA", @"ITAU",@"MARITIMA",@"MED SERVICE", @"SUL AMERICA",@"VOLKSWAGEN"];
+    [convenioHosp addObject:convenios];
+    convenios = @[@"ALIANZ",@"AMIL",@"BRADESCO",@"CARE PLUS",@"GOLDEN CROSS", @"GREEN LINE",@"MARITIMA",@"MEDIAL",@"MED SERVICE",@"PORTO SEGURO", @"SUL AMERICA",@"VOLKSWAGEN"];
+    [convenioHosp addObject:convenios];
+    convenios = @[@"ALIANZ",@"GAMA SAUDE",@"GOLDEN CROSS", @"INTERMEDICA",@"MARITIMA",@"MEDIAL",@"MED SERVICE",@"PORTO SEGURO", @"SUL AMERICA",@"VOLKSWAGEN"];
+    [convenioHosp addObject:convenios];
+    
     
 }
 
